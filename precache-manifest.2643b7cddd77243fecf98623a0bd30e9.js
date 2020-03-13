e35a3f71ff7a09e1cca747c2626a08d65fe7e516
amoljore7 <amoljore7@gmail.com>
2020-03-13 15:17:17 +0530

    Updates

---
+++ precache-manifest.2643b7cddd77243fecf98623a0bd30e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86224187c00615d25cb7060eeca08ef6",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/index.html"
+  },
+  {
+    "revision": "d5dca61d42f241e1a933",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/css/2.9adfe1b7.chunk.css"
+  },
+  {
+    "revision": "78a66857c8d069ad395b",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/css/main.e641689c.chunk.css"
+  },
+  {
+    "revision": "d5dca61d42f241e1a933",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/js/2.d264ab43.chunk.js"
+  },
+  {
+    "revision": "78a66857c8d069ad395b",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/js/main.8a2634e4.chunk.js"
+  },
+  {
+    "revision": "a8f9a25e93ca25feb0e0",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/js/runtime~main.c4039ed4.js"
+  },
+  {
+    "revision": "55d5567d5080cf9a8f294bad872ffc08",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/media/blur-background-1187974.55d5567d.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/31-Weather-app-Using-React-JS--Weather-API/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
