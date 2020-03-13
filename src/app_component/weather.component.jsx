a6954866569c787bee84d26f88ea2b280f64334a
amoljore7 <amoljore7@gmail.com>
2020-03-13 15:10:03 +0530

    WIP - fetched data dynamically and display on UI

--- src/app_component/weather.component.jsx
+++ src/app_component/weather.component.jsx
@@ -1,29 +1,42 @@
-import React from 'react';
+import React from "react";
+import "./weather.style.css";
 
+const Weather = props => {
+  return (
+    <div className="container text-light">
+      <div className="Card">
+        <h1 className="text-white py-3">{props.cityname}</h1>
+        <h5 className="py-4">
+          <i className={`wi ${props.weatherIcon} display-1`} />
+        </h5>
 
-const Weather = (props) => {
-    return (
-        <div className="container">
-            <div className="cards">
-                <h1>{props.city}, {props.country}</h1>
-                <h5 className="py-4">
-                    <i className={`wi ${props.icon} display-1`}></i>
-                </h5>
-                <h1 className="py-2">{props.celsius}&deg;</h1>
-                {minmaxTemp(props.temp_min, props.temp_max, props.description)}
-            </div>
-        </div>
-    );
+        {/* Get Celsius */}
+        {props.temp_celsius ? (
+          <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+        ) : null}
+
+        {/* show max and min temp */}
+        {maxminTemp(props.temp_min, props.temp_max)}
+
+        {/* Weather description */}
+        <h4 className="py-3">
+          {props.description.charAt(0).toUpperCase() +
+            props.description.slice(1)}
+        </h4>
+      </div>
+    </div>
+  );
 };
 
-function minmaxTemp(min, max,description) {
-    return (
-        <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
-            <h3 className="py3">{description}</h3>
-        </h3>
-    )
-}
-
 export default Weather;
+
+function maxminTemp(min, max) {
+  if (max && min) {
+    return (
+      <h3>
+        <span className="px-4">{min}&deg;</span>
+        <span className="px-4">{max}&deg;</span>
+      </h3>
+    );
+  }
+}
